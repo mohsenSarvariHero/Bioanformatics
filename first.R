c878e764e0d01e9e32a0618f06b5fcf0684084b2
mohsenSarvari <mohsen_sarvari1379@yahoo.com>
2024-02-22 13:50:58 +0330

    first code R in Bio

---
+++ first.R
@@ -0,0 +1,54 @@
+name <- "mohsen"
+name <- "mobin"
+var1 <- var2 <- var3 <- "sarvari"
+class(var1)
+class(name)
+y <- 10
+x <- 4
+z <- y+x
+z
+z <- y-x
+z
+z <- y**x
+z
+sqrt(16)
+abs(-201)
+min(20,30,1,0,-100)
+max(100,20,0,-1000)
+ceiling(2.1)
+floor(2.99)
+"lke"
+str1 <- "molkjldjc , kdk;,;ldkklkdk,klkvjkhvh;a
+dlkkkffr frfkjjfkj jfkj jkjhfg,
+fkldldflk;kjm kjlfn nklnv,
+kjdkfjdkflkm."
+str1
+cat(str1)
+
+sttrr <- "mohsen sarvari ."
+nchar("mohsen sarvari .")
+grepl("h",sttrr)
+grepl("hs",sttrr)
+grepl("hn",sttrr)
+grepl("ho",sttrr)
+name1 <- "moh"
+name2 <- "sen"
+paste(name1,name2,sep="")
+paste(name1,name2,end="*")
+
+
+
+5>10
+3<1
+2>1
+2=2
+2==2
+x <- 10
+y <- 8
+if (x<y){
+  print("y is bigger than x")
+  
+}else{
+  print("x is smaller than y")
+}
+getwd()
